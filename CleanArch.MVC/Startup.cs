6ba8ed518e2d62755456e7448d29ec6657a33fa3
José Eduardo Jesus <jose.eduardo@supermercadosbh.com.br>
2023-07-10 10:17:55 -0300

    Implementando Clean Arch pt.2

--- CleanArch.MVC/Startup.cs
+++ CleanArch.MVC/Startup.cs
@@ -1,4 +1,4 @@
-using CleanArch.MVC.Data;
+using CleanArch.Infra.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -22,15 +22,12 @@ namespace CleanArch.MVC {
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+        public void ConfigureServices(IServiceCollection services) 
+            
+        {
+            services.AddInfraestructure(Configuration);
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
